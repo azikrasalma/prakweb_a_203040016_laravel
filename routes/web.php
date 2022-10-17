73c2e9e5179b7ae1e4690b52a106edb77d9bf69c
azikrasalma <azikrasalma352@gmail.com>
2022-10-17 22:18:05 +0700

    menyelesaikan video 4

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,57 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/Home', function () {
-    return view ('home');
-});
-
-Route::get('/about', function () {
-    return view ('about', [
-        "name" => "Azikra Salma Sabilla",
-        "email" => "azikrasalma@gmail.com",
-        "image" => "jikra.jfif"
-
+Route::get('/', function () {
+    return view('Home',[
+        "title"=>"Home"
     ]);
 });
 
-Route::get('/posts', function () {
-    return view ('posts');
+
+Route::get('/about', function () {
+    return view('about',[
+    "title"=>"About",
+    "name"=>"Azikra Salma Sabilla",
+    "email"=>"azikrasalma@gmail.com",
+    "image"=>"jikra.jfif"
+    ]
+);
+});
+
+Route::get('/blog', function () {
+    $blog_posts =[
+        [
+            "title"=> "Judul Post Pertama",
+            "slug"=>"Judul Post Pertama",
+            "autor"=> "Azikra Salma Sabilla",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+        ];
+    
+    return view('posts',[
+        "title"=>"Posts",
+        "posts"=>$blog_posts
+    ]);
+});
+
+//halaman  
+Route::get('posts/{slug}',function($slug){
+    $blog_posts =[
+        [
+            "title"=> "Judul Post Pertama",
+            "slug"=>"Judul Post Pertama",
+            "autor"=> "Azikra Salma Sabilla",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+        ];foreach ($blog_posts as $post) {
+            if ($post["slug"] === $slug) {
+                $new_post = $post;
+            }
+        }
+
+
+    return view('post',[
+        "title" => "Single Post",
+        "post" =>$new_post
+    ]);
 });
