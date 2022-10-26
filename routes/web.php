574473344890e7cba22382aded0761669a7aa8e9
azikrasalma <azikrasalma352@gmail.com>
2022-10-26 23:34:41 +0700

    menyelesaikan video 11 laravel

--- routes/web.php
+++ routes/web.php
@@ -37,26 +37,24 @@ Route::get('/blog', [PostController::class, 'index']);
 
 //halaman  
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::get('/categories', function() {
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'title' => "Post by Category: $category->name",
+        'posts' => $category->posts->load('category', 'author')
+    ]);
+});
+
+
+Route::get('/categories/', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
     ]);
 });
 
-Route::get('categories/{category:slug}', function(Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name,
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
+Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts,
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
-
-
