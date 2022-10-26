ba99854e9c33e6858fa548bc51541d220b5bbe87
azikrasalma <azikrasalma352@gmail.com>
2022-10-26 18:30:12 +0700

    menyelesaikan video 7 laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-    <h2>{{ $post["title"] }}</h2>
-    <h5>{{ $post["autor"] }}</h5>
-    <p>{{ $post["Body"] }}</p>
-</article>
+    <h2>{{ $post->title }}</h2>
+
+    {!! $post->body !!}
 
 <a href="/blog">Back</a>
 @endsection
